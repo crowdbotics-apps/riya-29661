a977ef3da313aeb2e873aec628a3a68e1043a3de
Crowdbotics <team@crowdbotics.com>
2021-08-09 11:38:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "riya_29661Identifier",
+  url: "https://riya_29661Identifier.botics.co",
+  api: "https://riya_29661Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
